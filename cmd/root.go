fbf2759eee44f5b9a7b66c62a7afc23cfc750623
saltbo <saltbo@foxmail.com>
2022-11-13 23:11:50 +0800

    fix: update the kubeconfig
    Signed-off-by: saltbo <saltbo@foxmail.com>

--- cmd/root.go
+++ cmd/root.go
@@ -79,6 +79,8 @@ func initConfig() {
 		// viper.SetConfigName(".kiae")
 	}
 
+	viper.AutomaticEnv() // read in environment variables that match
+
 	// If a config file is found, read it in.
 	if err := viper.ReadInConfig(); err == nil {
 		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
