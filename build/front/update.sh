e83ba8fbd8a9c1189b3d75788c25c026dc23542f
saltbo <saltbo@foxmail.com>
2023-01-03 21:41:27 +0800

    chore: update the front to v0.0.13
    Signed-off-by: saltbo <saltbo@foxmail.com>

--- build/front/update.sh
+++ build/front/update.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-FRONT_VERSION="v0.0.12"
+FRONT_VERSION="v0.0.13"
 
 curl -Lf https://github.com/kiaedev/kiae-front/releases/download/${FRONT_VERSION}/kiae-front-dist.tar.gz -o kiae-front-dist.tar.gz
 tar zxvf kiae-front-dist.tar.gz && rm -rf kiae-front-dist.tar.gz
